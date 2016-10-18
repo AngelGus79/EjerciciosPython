d93fbef4126b450f0e5bccb96a2fe3518f02dbfd
AngelGus79 <agustavo79@gmail.com>
2016-10-17 22:44:50 -0500

    Se agrego archivo UnpackingaSequenceintoSeparateVariables.py

---
+++ UnpackingaSequenceintoSeparateVariables.py
@@ -0,0 +1,24 @@
+#tupla
+p = (4,5)
+x, y = p
+print(x)
+print(y)
+
+#Lista
+arreglo = ['h', 'o', 'l', 'a', 1,(17,10,2016)]
+a,b,c,d,r,fecha = arreglo
+print(fecha)
+print(fecha[0])
+
+#Tupla dentro de lista
+a,b,c,d,r,(dia, mes, ano) = arreglo
+print("mes: " + str(mes))
+
+#Si no coincide el numero de variables con el numero de iterables manda error
+
+#con cadena, puede ser cualquier iterable
+a,s,d,f = "hola"
+
+print(a)
+
+_,s,d,f = "hola"
