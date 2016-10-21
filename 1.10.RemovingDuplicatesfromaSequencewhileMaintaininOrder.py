fda22c3a8412240a51049f1b4abfac5268029fcc
AngelGus79 <agustavo79@gmail.com>
2016-10-20 19:32:30 -0500

    Update 1.10.RemovingDuplicatesfromaSequencewhileMaintaininOrder.py

--- 1.10.RemovingDuplicatesfromaSequencewhileMaintaininOrder.py
+++ 1.10.RemovingDuplicatesfromaSequencewhileMaintaininOrder.py
@@ -1,5 +1,5 @@
 """
-En este ejemplo se muestra como eliminar duplicados mientras se manteniendo
+En este ejemplo se muestra como eliminar duplicados mientras se mantiene
 el orden del resto de los elementos.
 Esto ya es posible hacerlo usando set, pero el detalle con esto es que:
 1.-No se puede hacer en elementos genericos
@@ -23,7 +23,7 @@ print(set(a)) #{1, 2, 10, 5, 9} directamente no guarda ningun tipo de orden
 si se intentan guardar duplicados en un tipo unhashable como los diccionarios. Esta funcion
 tambien puede servir para datos genericos
 
-key es para programar funciones lambda
+en key se pueden programar funciones lambda
 """
 
 def dedupe(items, key=None):
